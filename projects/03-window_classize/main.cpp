e153d2239bfc15a59ec1400ea2795b527487d819
izaki <dev4st@gmail.com>
2013-10-31 20:13:33 +0900

    add window classize

---
+++ projects/03-window_classize/main.cpp
@@ -0,0 +1,52 @@
+﻿#pragma comment(lib , "pdcurses.lib")
+
+#include "window.h"
+#include <curses.h>
+#include <string.h>
+
+#include <memory>
+#include <string>
+
+namespace curses {
+
+class Initializer {
+public:
+	Initializer(int y, int x) {
+		initscr();
+		start_color();
+		resize_term(y, x);
+	}
+
+	~Initializer() {
+		endwin();
+	}
+
+	void setColorPair(int no, int col, int bgCol) {
+		init_pair(no, col, bgCol);
+	}
+};
+
+}// namespace pdcurses
+
+using namespace std;
+
+int main() {
+	curses::Initializer ci(10, 40);
+	ci.setColorPair(1, COLOR_WHITE, COLOR_BLACK);
+	ci.setColorPair(2, COLOR_WHITE, COLOR_BLUE);
+	ci.setColorPair(3, COLOR_WHITE, COLOR_CYAN);
+
+	curses::Window::Ptr wnd = make_shared<curses::Window>(0, 0, COLS, LINES);
+	wnd->bkgd(COLOR_PAIR(1));
+	wnd->move(0, 0);
+	wnd->addch('a');
+     
+	// 中央揃え
+	wnd->move(((COLS-1)-strlen("Hello world!!")) / 2, 3);
+	wnd->addstr("Hello world!!");
+
+	wnd->refresh();
+
+	while (1) ;
+	return 0;
+}
---
+++ projects/03-window_classize/window.h
@@ -0,0 +1,65 @@
+﻿// ファイヤープロジェクト >> cursesの基本的な関数
+// http://www.fireproject.jp/feature/c-language/curses/basic.html
+// NCURSES Programming HOWTO
+// http://www.on-sky.net/~hs/misc/?NCURSES+Programming+HOWTO#l12
+// Ruby 1.9.3 リファレンスマニュアル >> cursesライブラリ
+// http://docs.ruby-lang.org/ja/1.9.3/class/Curses=3a=3aWindow.html
+
+#include <string>
+#include <memory>
+
+namespace curses {
+
+typedef unsigned long MCHAR;
+
+class Window {
+public:
+	typedef std::shared_ptr<Window> Ptr;
+	Window(int x, int y, int width, int height);
+	~Window();
+
+	int addstr(std::string);
+	int addch(char);
+	int attroff(MCHAR);
+	int attron(MCHAR);
+	int attrset(MCHAR);
+	int getbegx();
+	int getbegy();
+	int bkgd(MCHAR);
+	void bkgdset(MCHAR);
+	int box(char vertical_char, char horizontal_char);
+	int clear();
+	int clrtoeol();
+	int color_set(short, void*);
+	int curx();
+	int cury();
+	int delch();
+	int deleteln();
+	MCHAR getbkgd();
+	char getch();
+	//std::string getstr(); バッファオーバーフローするため非推奨
+	std::string getnstr(int len);// 読み込む文字数
+	int idlok(bool);
+	int inch();
+	int insch(char);
+	int insertln();
+	int keypad(int);
+	int maxx();
+	int maxy();
+	int move(int x, int y);
+	int nodelay(bool);
+	int refresh();
+	int resize(int lines, int cols);
+	int scrl(int num);
+	int scroll();
+	int scrollok(bool);
+	int setscrreg(int top, int bottom);
+	Ptr subwin(int height, int width, int y, int x);
+	void timeout(int);
+
+private:
+	class Impl;
+	std::shared_ptr<Impl> _impl;
+};
+
+}// namespace pdcurses
